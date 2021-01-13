b060a50772490d7046e070617a74da2d27c9a3c6
Kokhaviel <golinelli.adrien@gmail.com>
2021-01-13 16:49:24 +0100

    Add : Music Join Command

--- src/main/java/fr/kokhaviel/bot/Mee7.java
+++ src/main/java/fr/kokhaviel/bot/Mee7.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.security.auth.login.LoginException;
 
 import fr.kokhaviel.bot.commands.moderation.*;
+import fr.kokhaviel.bot.commands.music.JoinCommand;
 import fr.kokhaviel.bot.commands.server.RoleInfoCommand;
 import fr.kokhaviel.bot.commands.server.ServerInfoCommand;
 import net.dv8tion.jda.api.JDA;
@@ -27,27 +28,31 @@ import fr.kokhaviel.bot.commands.util.RepoCommand;
 import fr.kokhaviel.bot.commands.util.ShutdownCommand;
 import fr.kokhaviel.bot.event.logs.Logs;
 import fr.kokhaviel.bot.event.moderation.AutoModerator;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Mee7 {
 	
 	public static List<String> afkIDs = new ArrayList<>();
+
+	public static JDA jda;
 	
 	public Mee7() {
 		
 		try {
-			@SuppressWarnings("unused")
-			JDA jda = JDABuilder
-					.create(Config.BOT_TOKEN, 
+
+			jda = JDABuilder
+					.create(Config.BOT_TOKEN,
 							GatewayIntent.GUILD_MEMBERS,
 							GatewayIntent.GUILD_BANS,
 							GatewayIntent.GUILD_EMOJIS,
 							GatewayIntent.GUILD_INVITES,
 							GatewayIntent.GUILD_PRESENCES,
-							GatewayIntent.GUILD_MESSAGES, 
+							GatewayIntent.GUILD_MESSAGES,
 							GatewayIntent.GUILD_MESSAGE_REACTIONS,
 							GatewayIntent.GUILD_MESSAGE_TYPING,
 							GatewayIntent.GUILD_VOICE_STATES,
 							GatewayIntent.GUILD_WEBHOOKS)
+					.enableCache(CacheFlag.VOICE_STATE)
 					.addEventListeners(new Logs())
 					.addEventListeners(new AutoModerator())
 					.addEventListeners(new BanCommand(), new UnbanCommand(), new KickCommand(), new ClearCommand(), new MuteCommand(), new UnmuteCommand())
@@ -58,6 +63,7 @@ public class Mee7 {
 					.addEventListeners(new RandomCommand(), new SayCommand(), new EightBallCommand())
 					.addEventListeners(new RoleInfoCommand(), new ServerInfoCommand())
 					.addEventListeners(new MassKickCommand(), new MassBanCommand())
+					.addEventListeners(new JoinCommand())
 					.setActivity(Activity.watching("la doc avant de poser une question..."))
 					.build();
 		} catch (LoginException le) {
---
+++ src/main/java/fr/kokhaviel/bot/commands/music/JoinCommand.java
@@ -0,0 +1,60 @@
+package fr.kokhaviel.bot.commands.music;
+
+import fr.kokhaviel.bot.Config;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("ConstantConditions")
+public class JoinCommand extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        final TextChannel channel = (TextChannel) event.getChannel();
+        final Member selfMember = event.getGuild().getSelfMember();
+        final GuildVoiceState voiceState = selfMember.getVoiceState();
+        final Member member = event.getMember();
+        final GuildVoiceState memberVoiceState = member.getVoiceState();
+        final AudioManager audioManager = event.getGuild().getAudioManager();
+        final VoiceChannel memberChannel = memberVoiceState.getChannel();
+        final Message message = event.getMessage();
+
+        final String[] args = message.getContentRaw().split("\\s+");
+
+        if(args[0].equalsIgnoreCase(Config.MUSIC_PREFIX + "join"))
+
+        if(voiceState.inVoiceChannel()) {
+
+            message.delete().queueAfter(2, TimeUnit.SECONDS);
+
+            channel.sendMessage("I'm Already Connected To A Voice Channel !").queue(
+                    delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+
+        } else if(!memberVoiceState.inVoiceChannel()) {
+
+            message.delete().queueAfter(2, TimeUnit.SECONDS);
+
+            channel.sendMessage("You Need To Be In A Voice Channel For This Command Works !").queue(
+                    delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+        } else {
+
+            message.delete().queueAfter(2, TimeUnit.SECONDS);
+
+            audioManager.openAudioConnection(memberChannel);
+
+            channel.sendMessage("Successfully Connected To " + memberChannel.getName()).queue(
+                    delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+        }
+
+
+
+
+    }
+}
