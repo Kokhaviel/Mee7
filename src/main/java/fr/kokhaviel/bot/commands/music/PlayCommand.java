7e0597f7bcc965aca1dfa90fa20a75bf670586f6
Kokhaviel <golinelli.adrien@gmail.com>
2021-01-14 22:03:12 +0100

    Add : PlayCommand (That Plays Kim - Eminem)

---
+++ src/main/java/fr/kokhaviel/bot/commands/music/PlayCommand.java
@@ -0,0 +1,47 @@
+package fr.kokhaviel.bot.commands.music;
+
+import fr.kokhaviel.bot.Config;
+import fr.kokhaviel.bot.music.PlayerManager;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.concurrent.TimeUnit;
+
+public class PlayCommand extends ListenerAdapter {
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        final Message message = event.getMessage();
+        final String[] args = message.getContentRaw().split("\\s+");
+        final TextChannel channel = (TextChannel) event.getChannel();
+        final Member selfMember = event.getGuild().getSelfMember();
+        final GuildVoiceState selfVoiceState = selfMember.getVoiceState();
+        final Member member = event.getMember();
+        final GuildVoiceState voiceState = member.getVoiceState();
+
+
+        if(args[0].equalsIgnoreCase(Config.MUSIC_PREFIX + "play")) {
+
+            if(!selfVoiceState.inVoiceChannel()) {
+
+                message.delete().queueAfter(2, TimeUnit.SECONDS);
+
+                channel.sendMessage("I need to be in a voice channel to this command works !").queue(
+                        delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+            } else if(!voiceState.getChannel().equals(selfVoiceState.getChannel())) {
+
+                channel.sendMessage("You need to be in the same voice channel as me for this command works !").queue(
+                        delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+            } else {
+
+                PlayerManager.getInstance().loadAndPlay(channel, "https://www.youtube.com/watch?v=-SeVN2u5Axc");
+            }
+
+        }
+    }
+}
--- src/main/java/fr/kokhaviel/bot/music/PlayerManager.java
+++ src/main/java/fr/kokhaviel/bot/music/PlayerManager.java
@@ -79,7 +79,7 @@ public class PlayerManager {
 
     }
 
-    private static PlayerManager getInstance() {
+    public static PlayerManager getInstance() {
 
         if(INSTANCE == null) INSTANCE = new PlayerManager();
 
