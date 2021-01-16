fd90abc956c6b7a8ca1e213f6a26c3d775efc03a
Kokhaviel <golinelli.adrien@gmail.com>
2021-01-16 17:30:10 +0100

    Add : Pause Command

---
+++ src/main/java/fr/kokhaviel/bot/commands/music/PauseCommand.java
@@ -0,0 +1,72 @@
+package fr.kokhaviel.bot.commands.music;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import fr.kokhaviel.bot.Config;
+import fr.kokhaviel.bot.music.GuildMusicManager;
+import fr.kokhaviel.bot.music.PlayerManager;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("ConstantConditions")
+public class PauseCommand extends ListenerAdapter {
+
+    boolean isPaused = false;
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+
+        final Message message = event.getMessage();
+        final Member member = event.getMember();
+        final Guild guild = event.getGuild();
+        final Member selfMember = guild.getSelfMember();
+        final TextChannel channel = (TextChannel) event.getChannel();
+        final String[] args = message.getContentRaw().split("\\s+");
+
+        if (args[0].equalsIgnoreCase(Config.MUSIC_PREFIX + "pause")) {
+
+            final GuildVoiceState voiceState = member.getVoiceState();
+            final GuildVoiceState selfVoiceState = selfMember.getVoiceState();
+            final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(guild);
+            final BlockingQueue<AudioTrack> queue = musicManager.scheduler.queue;
+
+
+            message.delete().queue();
+
+            if (!voiceState.inVoiceChannel()) {
+
+                channel.sendMessage("You need to be in a voice channel to this command works").queue(
+                        delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+            } else if (!selfVoiceState.inVoiceChannel()) {
+
+                channel.sendMessage("I need to be in a voice channel to this command works !").queue(
+                        delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+            } else if (!voiceState.getChannel().equals(selfVoiceState.getChannel())) {
+
+                channel.sendMessage("You need to be in the same voice channel as me for this command works !").queue(
+                        delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+
+            } else {
+
+                musicManager.scheduler.player.setPaused(!this.isPaused);
+
+                if (!isPaused) {
+                    channel.sendMessage("Player has been paused !").queue(
+                            delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+                } else {
+                    channel.sendMessage("Player has been resumed !").queue(
+                            delete -> delete.delete().queueAfter(5, TimeUnit.SECONDS));
+                }
+
+                this.isPaused = !this.isPaused;
+
+            }
+        }
+    }
+}
