bd152b0e757dadb28de93bdb5dcb544914fc477e
Kokhaviel <golinelli.adrien@gmail.com>
2021-01-17 12:50:20 +0100

    Update : Reorganize Class

--- src/main/java/fr/kokhaviel/bot/Mee7.java
+++ src/main/java/fr/kokhaviel/bot/Mee7.java
@@ -7,78 +7,92 @@ import javax.security.auth.login.LoginException;
 
 import fr.kokhaviel.bot.commands.moderation.*;
 import fr.kokhaviel.bot.commands.music.*;
-import fr.kokhaviel.bot.commands.server.RoleInfoCommand;
-import fr.kokhaviel.bot.commands.server.ServerInfoCommand;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
+import fr.kokhaviel.bot.commands.server.*;
+import net.dv8tion.jda.api.*;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import fr.kokhaviel.bot.commands.fun.EightBallCommand;
-import fr.kokhaviel.bot.commands.fun.RandomCommand;
-import fr.kokhaviel.bot.commands.fun.SayCommand;
-import fr.kokhaviel.bot.commands.user.AvatarCommand;
-import fr.kokhaviel.bot.commands.user.InfoCommand;
+import fr.kokhaviel.bot.commands.fun.*;
+import fr.kokhaviel.bot.commands.user.*;
 import fr.kokhaviel.bot.commands.user.afk.AfkCommand;
 import fr.kokhaviel.bot.event.afk.AfkVerify;
-import fr.kokhaviel.bot.commands.util.AboutCommand;
-import fr.kokhaviel.bot.commands.util.HelpCommand;
-import fr.kokhaviel.bot.commands.util.PingCommand;
-import fr.kokhaviel.bot.commands.util.RebootCommand;
-import fr.kokhaviel.bot.commands.util.RepoCommand;
-import fr.kokhaviel.bot.commands.util.ShutdownCommand;
+import fr.kokhaviel.bot.commands.util.*;
 import fr.kokhaviel.bot.event.logs.Logs;
 import fr.kokhaviel.bot.event.moderation.AutoModerator;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Mee7 {
-	
-	public static List<String> afkIDs = new ArrayList<>();
 
-	public static JDA jda;
-	
-	public Mee7() {
-		
-		try {
+    public static List<String> afkIDs = new ArrayList<>();
 
-			jda = JDABuilder
-					.create(Config.BOT_TOKEN,
-							GatewayIntent.GUILD_MEMBERS,
-							GatewayIntent.GUILD_BANS,
-							GatewayIntent.GUILD_EMOJIS,
-							GatewayIntent.GUILD_INVITES,
-							GatewayIntent.GUILD_PRESENCES,
-							GatewayIntent.GUILD_MESSAGES,
-							GatewayIntent.GUILD_MESSAGE_REACTIONS,
-							GatewayIntent.GUILD_MESSAGE_TYPING,
-							GatewayIntent.GUILD_VOICE_STATES,
-							GatewayIntent.GUILD_WEBHOOKS)
-					.enableCache(CacheFlag.VOICE_STATE)
-					.addEventListeners(new Logs())
-					.addEventListeners(new AutoModerator())
-					.addEventListeners(new BanCommand(), new UnbanCommand(), new KickCommand(), new ClearCommand(),
-							new MuteCommand(), new UnmuteCommand())
-					.addEventListeners(new PingCommand(), new RebootCommand(), new RepoCommand(), new ShutdownCommand())
-					.addEventListeners(new HelpCommand(), new AboutCommand())
-					.addEventListeners(new AfkCommand(), new AfkVerify())
-					.addEventListeners(new AvatarCommand(), new InfoCommand())
-					.addEventListeners(new RandomCommand(), new SayCommand(), new EightBallCommand())
-					.addEventListeners(new RoleInfoCommand(), new ServerInfoCommand())
-					.addEventListeners(new MassKickCommand(), new MassBanCommand())
-					.addEventListeners(new JoinCommand(), new PlayCommand(), new StopCommand(), new SkipCommand(),
-							new NowPlayingCommand(), new QueueCommand(),new RepeatCommand(), new LeaveCommand(),
-							new PauseCommand(), new VolumeCommand())
-					.setActivity(Activity.watching("la doc avant de poser une question..."))
-					.build();
-		} catch (LoginException le) {
-			le.printStackTrace();
-		}
-		
-	}
+    public static JDA jda;
 
-	
-	
-	public static void main(String[] args) {
+    public Mee7() {
 
-		new Mee7();
-	}
+        try {
+
+            jda = JDABuilder
+                    .create(Config.BOT_TOKEN,
+                            GatewayIntent.GUILD_MEMBERS,
+                            GatewayIntent.GUILD_BANS,
+                            GatewayIntent.GUILD_EMOJIS,
+                            GatewayIntent.GUILD_INVITES,
+                            GatewayIntent.GUILD_PRESENCES,
+                            GatewayIntent.GUILD_MESSAGES,
+                            GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                            GatewayIntent.GUILD_MESSAGE_TYPING,
+                            GatewayIntent.GUILD_VOICE_STATES,
+                            GatewayIntent.GUILD_WEBHOOKS)
+
+                    .enableCache(CacheFlag.VOICE_STATE)
+
+                    .addEventListeners(new AboutCommand())
+                    .addEventListeners(new AfkCommand())
+                    .addEventListeners(new AfkVerify())
+                    .addEventListeners(new AutoModerator())
+                    .addEventListeners(new AvatarCommand())
+                    .addEventListeners(new BanCommand())
+                    .addEventListeners(new ClearCommand())
+                    .addEventListeners(new EightBallCommand())
+                    .addEventListeners(new HelpCommand())
+                    .addEventListeners(new InfoCommand())
+                    .addEventListeners(new JoinCommand())
+                    .addEventListeners(new KickCommand())
+                    .addEventListeners(new LeaveCommand())
+                    .addEventListeners(new Logs())
+                    .addEventListeners(new MassKickCommand())
+                    .addEventListeners(new MassBanCommand())
+                    .addEventListeners(new MuteCommand())
+                    .addEventListeners(new NowPlayingCommand())
+                    .addEventListeners(new PauseCommand())
+                    .addEventListeners(new PingCommand())
+                    .addEventListeners(new PlayCommand())
+                    .addEventListeners(new QueueCommand())
+                    .addEventListeners(new RandomCommand())
+                    .addEventListeners(new RebootCommand())
+                    .addEventListeners(new RepeatCommand())
+                    .addEventListeners(new RepoCommand())
+                    .addEventListeners(new RoleInfoCommand())
+                    .addEventListeners(new SayCommand())
+                    .addEventListeners(new ServerInfoCommand())
+                    .addEventListeners(new ShutdownCommand())
+                    .addEventListeners(new SkipCommand())
+                    .addEventListeners(new StopCommand())
+                    .addEventListeners(new UnbanCommand())
+                    .addEventListeners(new UnmuteCommand())
+                    .addEventListeners(new VolumeCommand())
+
+                    .setActivity(Activity.watching("la doc avant de poser une question..."))
+
+                    .build();
+
+        } catch (LoginException le) {
+            le.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        new Mee7();
+    }
 }
