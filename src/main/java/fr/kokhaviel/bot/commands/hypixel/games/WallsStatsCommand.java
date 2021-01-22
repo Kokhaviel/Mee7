d1f97433e5161a219915e3280a610aa8e70f230c
Kokhaviel <golinelli.adrien@gmail.com>
2021-01-22 13:28:58 +0100

    Update : Recode Embed Messages

--- src/main/java/fr/kokhaviel/bot/commands/hypixel/games/WallsStatsCommand.java
+++ src/main/java/fr/kokhaviel/bot/commands/hypixel/games/WallsStatsCommand.java
@@ -43,57 +43,66 @@ public class WallsStatsCommand extends ListenerAdapter {
                     final Player player = sloth.getPlayer(args[1]);
                     final Walls walls = player.getStats().getWalls();
 
-                    EmbedBuilder wallsEmbed = new EmbedBuilder();
-                    wallsEmbed.setAuthor("Walls Stats", null, "https://cdn.discordapp.com/icons/489529070913060867/b8fe7468a1feb1020640c200313348b0.webp?size=128");
-                    wallsEmbed.setColor(Color.YELLOW);
-                    wallsEmbed.setTitle(player.getUsername() + " Stats");
-                    wallsEmbed.setFooter("Developped by " + Config.DEVELOPER_TAG + "\nAction Generated on " + event.getGuild().getName(), "https://cdn.discordapp.com/avatars/560156789178368010/790bd41a9474a82b20ca813f2be49641.webp?size=128");
-
-                    wallsEmbed.addField("Coins : ", String.valueOf(walls.getCoins()), true);
-
-                    wallsEmbed.addBlankField(false);
-                    wallsEmbed.addField("Kills : ", String.valueOf(walls.getKills()), true);
-                    wallsEmbed.addField("Deaths : ", String.valueOf(walls.getDeaths()), true);
-                    wallsEmbed.addField("Assists : ", String.valueOf(walls.getAssists()), true);
-                    wallsEmbed.addField("KDR : ", String.valueOf(walls.getKd()), true);
-                    wallsEmbed.addField("Wins :", String.valueOf(walls.getWins()), true);
-                    wallsEmbed.addField("Losses : ", String.valueOf(walls.getLosses()), true);
-                    wallsEmbed.addField("WinLoss : ", String.valueOf(walls.getWinLoss()), true);
-                    wallsEmbed.addField("Win Percentetage : ", String.valueOf(walls.getWinPercentage()), true);
-
-                    EmbedBuilder wallsPerksEmbed = new EmbedBuilder();
-                    wallsPerksEmbed.setAuthor("Walls Perks", null, "https://cdn.discordapp.com/icons/489529070913060867/b8fe7468a1feb1020640c200313348b0.webp?size=128");
-                    wallsPerksEmbed.setColor(Color.YELLOW);
-                    wallsPerksEmbed.setTitle(player.getUsername() + " Stats");
-                    wallsPerksEmbed.setFooter("Developped by " + Config.DEVELOPER_TAG + "\nAction Generated on " + event.getGuild().getName(), "https://cdn.discordapp.com/avatars/560156789178368010/790bd41a9474a82b20ca813f2be49641.webp?size=128");
-
-                    wallsPerksEmbed.addField("Boss Kills Perk : ", String.valueOf(walls.getPerks().getBossSkills()), true);
-                    wallsPerksEmbed.addField("Boss Digger Perk : ", String.valueOf(walls.getPerks().getBossDigger()), true);
-                    wallsPerksEmbed.addField("Boss Guardian Perk : ", String.valueOf(walls.getPerks().getBossGuardian()), true);
-                    wallsPerksEmbed.addField("Scotman Perk : ", String.valueOf(walls.getPerks().getScotsman()), true);
-                    wallsPerksEmbed.addField("Insane Farmer Perk : ", String.valueOf(walls.getPerks().getInsaneFarmer()), true);
-                    wallsPerksEmbed.addField("Thats Hot Perk : ", String.valueOf(walls.getPerks().getThatsHot()), true);
-                    wallsPerksEmbed.addField("Gold Rush Perk : ", String.valueOf(walls.getPerks().getGoldRush()), true);
-                    wallsPerksEmbed.addField("Leather Worker Perk : ", String.valueOf(walls.getPerks().getLeatherWorker()), true);
-                    wallsPerksEmbed.addField("Soup Drinker Perk : ", String.valueOf(walls.getPerks().getSoupDrinker()), true);
-                    wallsPerksEmbed.addField("Really Shiny Perk : ", String.valueOf(walls.getPerks().getReallyShiny()), true);
-                    wallsPerksEmbed.addField("Pyromaniac Perk : ", String.valueOf(walls.getPerks().getPyromaniac()), true);
-                    wallsPerksEmbed.addField("Einstein Perk : ", String.valueOf(walls.getPerks().getEinstein()), true);
-                    wallsPerksEmbed.addField("Skybase King Perk : ", String.valueOf(walls.getPerks().getSkybaseKing()), true);
-                    wallsPerksEmbed.addField("Burn Baby Burn Perk : ", String.valueOf(walls.getPerks().getBurnBabyBurn()), true);
-                    wallsPerksEmbed.addField("Very Fortunate Perk : ", String.valueOf(walls.getPerks().getVeryFortunate()), true);
-                    wallsPerksEmbed.addField("Get To The Choppar Perk : ", String.valueOf(walls.getPerks().getGetToTheChoppa()), true);
-                    wallsPerksEmbed.addField("Champion Perk : ", String.valueOf(walls.getPerks().getChampion()), true);
-
-
-                    channel.sendMessage(wallsEmbed.build()).queue();
-                    channel.sendMessage(wallsPerksEmbed.build()).queue();
+                    channel.sendMessage(getWallsStats(event, player, walls).build()).queue();
+                    channel.sendMessage(getWallsPerksStats(event, player, walls).build()).queue();
 
                 }
             }
 
         }
 
+    }
+
+    private EmbedBuilder getWallsStats(MessageReceivedEvent event, Player player, Walls walls) {
+
+        EmbedBuilder wallsEmbed = new EmbedBuilder();
+        wallsEmbed.setAuthor("Walls Stats", null, "https://cdn.discordapp.com/icons/489529070913060867/b8fe7468a1feb1020640c200313348b0.webp?size=128");
+        wallsEmbed.setColor(Color.YELLOW);
+        wallsEmbed.setTitle(player.getUsername() + " Stats");
+        wallsEmbed.setFooter("Developped by " + Config.DEVELOPER_TAG + "\nAction Generated on " + event.getGuild().getName(), "https://cdn.discordapp.com/avatars/560156789178368010/790bd41a9474a82b20ca813f2be49641.webp?size=128");
+
+        wallsEmbed.addField("Coins : ", String.valueOf(walls.getCoins()), true);
+
+        wallsEmbed.addBlankField(false);
+        wallsEmbed.addField("Kills : ", String.valueOf(walls.getKills()), true);
+        wallsEmbed.addField("Deaths : ", String.valueOf(walls.getDeaths()), true);
+        wallsEmbed.addField("Assists : ", String.valueOf(walls.getAssists()), true);
+        wallsEmbed.addField("KDR : ", String.valueOf(walls.getKd()), true);
+        wallsEmbed.addField("Wins :", String.valueOf(walls.getWins()), true);
+        wallsEmbed.addField("Losses : ", String.valueOf(walls.getLosses()), true);
+        wallsEmbed.addField("WinLoss : ", String.valueOf(walls.getWinLoss()), true);
+        wallsEmbed.addField("Win Percentetage : ", String.valueOf(walls.getWinPercentage()), true);
+
+        return wallsEmbed;
+    }
+
+    private EmbedBuilder getWallsPerksStats(MessageReceivedEvent event, Player player, Walls walls) {
+
+        EmbedBuilder wallsPerksEmbed = new EmbedBuilder();
+        wallsPerksEmbed.setAuthor("Walls Perks", null, "https://cdn.discordapp.com/icons/489529070913060867/b8fe7468a1feb1020640c200313348b0.webp?size=128");
+        wallsPerksEmbed.setColor(Color.YELLOW);
+        wallsPerksEmbed.setTitle(player.getUsername() + " Stats");
+        wallsPerksEmbed.setFooter("Developped by " + Config.DEVELOPER_TAG + "\nAction Generated on " + event.getGuild().getName(), "https://cdn.discordapp.com/avatars/560156789178368010/790bd41a9474a82b20ca813f2be49641.webp?size=128");
+
+        wallsPerksEmbed.addField("Boss Kills Perk : ", String.valueOf(walls.getPerks().getBossSkills()), true);
+        wallsPerksEmbed.addField("Boss Digger Perk : ", String.valueOf(walls.getPerks().getBossDigger()), true);
+        wallsPerksEmbed.addField("Boss Guardian Perk : ", String.valueOf(walls.getPerks().getBossGuardian()), true);
+        wallsPerksEmbed.addField("Scotman Perk : ", String.valueOf(walls.getPerks().getScotsman()), true);
+        wallsPerksEmbed.addField("Insane Farmer Perk : ", String.valueOf(walls.getPerks().getInsaneFarmer()), true);
+        wallsPerksEmbed.addField("Thats Hot Perk : ", String.valueOf(walls.getPerks().getThatsHot()), true);
+        wallsPerksEmbed.addField("Gold Rush Perk : ", String.valueOf(walls.getPerks().getGoldRush()), true);
+        wallsPerksEmbed.addField("Leather Worker Perk : ", String.valueOf(walls.getPerks().getLeatherWorker()), true);
+        wallsPerksEmbed.addField("Soup Drinker Perk : ", String.valueOf(walls.getPerks().getSoupDrinker()), true);
+        wallsPerksEmbed.addField("Really Shiny Perk : ", String.valueOf(walls.getPerks().getReallyShiny()), true);
+        wallsPerksEmbed.addField("Pyromaniac Perk : ", String.valueOf(walls.getPerks().getPyromaniac()), true);
+        wallsPerksEmbed.addField("Einstein Perk : ", String.valueOf(walls.getPerks().getEinstein()), true);
+        wallsPerksEmbed.addField("Skybase King Perk : ", String.valueOf(walls.getPerks().getSkybaseKing()), true);
+        wallsPerksEmbed.addField("Burn Baby Burn Perk : ", String.valueOf(walls.getPerks().getBurnBabyBurn()), true);
+        wallsPerksEmbed.addField("Very Fortunate Perk : ", String.valueOf(walls.getPerks().getVeryFortunate()), true);
+        wallsPerksEmbed.addField("Get To The Choppar Perk : ", String.valueOf(walls.getPerks().getGetToTheChoppa()), true);
+        wallsPerksEmbed.addField("Champion Perk : ", String.valueOf(walls.getPerks().getChampion()), true);
+
+        return wallsPerksEmbed;
     }
 
 }
